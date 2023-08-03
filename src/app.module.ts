a8ed1bc462fa5b626e6e3986ff5b257ec35a48f1
den <denissytnikmacho@gmail.com>
2023-08-03 12:10:34 +0300

    feat: added MongooseModule imports to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { ShortlinkController } from './shortlink/shortlink.controller';
 import { ShortlinkService } from './shortlink/shortlink.service';
 import { ShortlinkModule } from './shortlink/shortlink.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Shortlink, ShortlinkSchema } from './shortlink/schemas/shortlink.schema';
 
 @Module({
-  imports: [ShortlinkModule],
+  imports: [
+    ShortlinkModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://crazysparrow:564793@cluster0.8aeyxsx.mongodb.net/url-shortener?retryWrites=true&w=majority',
+    ),
+    MongooseModule.forFeature([
+      { name: Shortlink.name, schema: ShortlinkSchema },
+    ]),
+  ],
   controllers: [AppController, ShortlinkController],
   providers: [AppService, ShortlinkService],
 })
